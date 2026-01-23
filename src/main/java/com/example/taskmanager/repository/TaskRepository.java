ecc591f0896e05f582b8ec6881c6a859a12b3427
ipesterev <89259112970@mail.ru>
2026-01-23 12:58:35 +0300

    add some fix

--- src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -1,3 +1,5 @@
+package com.example.taskmanager.repository;
+
 import com.example.taskmanager.model.Task;
 import com.example.taskmanager.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
